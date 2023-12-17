3952055f25a760199095781d5b9f546bef9ccc68
Cristiano Leite <cristianosleite@hotmail.com>
2023-12-17 13:02:36 -0300

    Update dashboard

--- dashboard/src/app/alarm-history/alarm-history.component.ts
+++ dashboard/src/app/alarm-history/alarm-history.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-alarm-history',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './alarm-history.component.html',
   styleUrl: './alarm-history.component.scss'
 })
-export class AlarmHistoryComponent {
 
+export class AlarmHistoryComponent {
+  isLoading: boolean = true;
+  records: any[] = [
+    {
+      id: 1,
+      location: 'Setor G1',
+      recognizes: 'false',
+      eventDate: '17-12-2023',
+      activatedAt: '11:00:00',
+      deactivatedAt: '12:00:00',
+    }
+  ];
+
+  constructor() {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 500);
+  }
 }
