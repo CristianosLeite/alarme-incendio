0e518f46969fd8cbba8eae9b976e92e30474d316
Cristiano Leite <cristianosleite@hotmail.com>
2023-12-17 08:51:11 -0300

    Update dashboard.component.ts

--- dashboard/src/app/dashboard/dashboard.component.ts
+++ dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../components/header/header.component';
 import { DeviceComponent } from '../components/device/device.component';
 import { GridComponent } from '../components/grid/grid.component';
 import { StatusComponent } from '../components/status/status.component';
-import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -16,6 +14,4 @@ import { FooterComponent } from '../components/footer/footer.component';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
